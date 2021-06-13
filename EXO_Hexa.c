9db79c1206c997da05d6cd156e35970f1cbbdc9f
MortierEsteban <root@DESKTOP-25L5MA7.localdomain>
2021-06-13 21:30:55 +0200

    tout fonctionne c'est parfait

--- EXO_Hexa.c
+++ EXO_Hexa.c
@@ -2,14 +2,18 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-int main (const char* temp_str)
+void main (int argc,const char* argv[])
 {
+	if (argc==2)
+	{
 	//char *temp_str;
-	temp_str=(char*)malloc(150);
+//	argv[1]=(char*)malloc(150);
 	unsigned int nb=42;
 	//scanf("%s",temp_str);
-        nb=(atoi(temp_str));
+        nb=(atoi(argv[1]));
 //	free(temp_str);	
-	printf("%X \n",nb);
-	return(" %x",nb);
+	printf("argc=%d\n",argc);
+	printf("0x%08X",nb);
+	}
 }
+
